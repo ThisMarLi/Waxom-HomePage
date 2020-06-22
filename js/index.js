255ed9d298f6cbd707a7a03f20097f7e7c144b76
Marina Litvin <litvinms94@gmail.com>
2020-06-22 12:18:38 +0500

    Waxom HomePage

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    $(function () {
+        $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+
+    $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+         Counter: $(this).text()
+         }, {
+          duration: 1500,
+          easing: 'swing',
+          step: function (now) {
+             $(this).text(Math.ceil(now));
+          }
+         });
+     });
+
+     $(function(){
+        $('a[href^="#"]').on('click', function(event) {
+          // отменяем стандартное действие
+          event.preventDefault();
+          
+          var sc = $(this).attr("href"),
+              dn = $(sc).offset().top;
+          /*
+          * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+          * dn - определяем положение блока на странице
+          */
+          
+          $('html, body').animate({scrollTop: dn}, 500);
+          
+          /*
+          * 1000 скорость перехода в миллисекундах
+          */
+        });
+      });
+}); 
